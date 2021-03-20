be6e4e785c4b2333dde10c712172c2fc46f78e13
hectic97 <hecticlife@g.skku.edu>
2021-03-20 14:25:49 +0900

    edit file tree

---
+++ react_hook/useInput/useInput.js
@@ -0,0 +1,18 @@
+export const useInput = (initialValue, validator) => {
+    const [value, setValue] = useState(initialValue);
+    const onChange = (event) => {
+      console.log(event);
+      const {
+        target: { value }
+      } = event;
+      // const value = event.target.value;
+      let willUpdate = true;
+      if (typeof validator === "function") {
+        willUpdate = validator(value);
+      }
+      if (willUpdate) {
+        setValue(value);
+      }
+    };
+    return { value, onChange };
+  };
